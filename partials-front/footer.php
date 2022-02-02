90edb7cdde8e9a99e2dbe2eb9881efb9c0fcf51c
Mosarrat Shazia Kabir <81244301+mosarrat@users.noreply.github.com>
2022-02-03 01:41:39 +0600

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,29 @@
+    <!--social section starts here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/ios-filled/50/26e07f/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/ios-filled/50/26e07f/instagram.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/ios-filled/50/26e07f/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!--social section ends here -->
+
+    <!--footer section starts here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All Rights Reserved. Designed By <a href="#"><b>CSE299 Project Team</b></a></p>
+        </div>
+    </section>
+    <!--footer section ends here -->
+
+
+</body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,51 @@
+<?php 
+    
+    include("admin/connection.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!--website responsive-->>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Event Organizer</title>
+
+    <!--link css-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="review.css">
+</head>
+<body>
+    <!--Navbar section starts here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <img src="images/logo.png" alt="Organization logo" class="img-responsive">
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="category.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="event.php">Events</a>
+                    </li>
+                    <li>
+                        <a href="review.php">Reviews</a>
+                    </li>
+                    <li>
+                        <a href="cancel.php">Cancel</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!--Navbar section ends here -->
